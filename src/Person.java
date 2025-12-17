aa3afae690482471f8e04369ce6f9217a0b85627
Konex25 <rapacz.konrad@gmail.com>
2025-12-17 14:44:20 +0100

    Fix person encapsulation

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,20 @@
 public abstract class Person {
-    protected String name;
-    protected int age;
+    private String name;
+    private int age;
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
     }
 
     // Simple intro

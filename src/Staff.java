875cba9b842604976990356d8c9344063491a432
Konex25 <rapacz.konrad@gmail.com>
2025-12-17 14:53:48 +0100

    Fix Staff

--- src/Staff.java
+++ src/Staff.java
@@ -1,9 +1,18 @@
 public abstract class Staff extends Person {
-    protected String role;
+    private String role;
 
     public Staff(String name, int age, String role) {
         super(name, age);
         this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("Name: " + getName() + " | Age: " + getAge() + " | Role: " + role);
     }
 
     public abstract void performDuties();

d8f1db50af238d321030b5800a421e9982c6bb32
Konex25 <rapacz.konrad@gmail.com>
2025-12-17 13:57:42 +0100

    Person abstract class

---
+++ src/Person.java
@@ -0,0 +1,14 @@
+public abstract class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Simple intro
+    public void introduce() {
+        System.out.println("Name: " + name + "Age: " + age + " years old.");
+    }
+}
